ed39758b702d13982f80983cd70e5484dd155e2e
mongsil1025 <sunminjsm@gmail.com>
2022-03-20 22:22:09 +0900

    Added storybook adon

--- src/stories/InputStory.jsx
+++ src/stories/InputStory.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Input from '../03/Input';
+import { action } from '@storybook/addon-actions';
 
 storiesOf('Input', module)
   .add('기본 설정', () => <Input />)
-  .add('label 예제', () => <Input name="name" label="이름" />);
+  .add('label 예제', () => <Input name="name" label="이름" />)
+  .add('onChange 예제', () => <Input name="name" onChange={action('onChange 이벤트 발생')} />);
