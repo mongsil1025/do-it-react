efac722ee8d10987e551681b968c32f4355bbe76
mongsil1025 <sunminjsm@gmail.com>
2022-03-20 23:02:47 +0900

    Added Style Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 import { configure } from '@storybook/react';
 import interopRequireDefault from 'babel-runtime/helpers/interopRequireDefault';
+import '../src/sass/materialize.scss';
 
 function loadStories() {
   const context = require.context('../src/stories', true, /Story\.jsx$/);
